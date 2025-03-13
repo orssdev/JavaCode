11b360ad837bbfe129ca04d38c1db39fb7ccb240
Oscar <orss4123@gmail.com>
2025-03-13 19:37:18 -0400

    Code

---
+++ Code/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        String str = "Hello my name is %s and something else\n";
+        
+        System.out.printf(str, "Oscar");
+    }
+}
