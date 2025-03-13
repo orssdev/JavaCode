35de3eb0324f0e91e1e7ddb6ecc253df2d60d914
Oscar <orss4123@gmail.com>
2025-03-12 20:14:10 -0400

    FXML Progress?

--- Weather/app/src/main/java/dev/orss/weather/App.java
+++ Weather/app/src/main/java/dev/orss/weather/App.java
@@ -12,12 +12,14 @@ public class App extends Application
     public void start(Stage stage) throws Exception
     {
         stage.setTitle("Weather");
+        stage.setResizable(false);
         Image icon = new Image(getClass().getResourceAsStream("/images/weather-app.png"));
         stage.getIcons().add(icon);
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/UI/Main.fxml"));
         Parent root = loader.load();
 
         Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/UI/Main.css").toExternalForm());
 
         stage.setScene(scene);
         stage.show();
